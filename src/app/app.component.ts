6f8682e550c6a4ca91b529ad127a6da1699a53b4
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-05-13 21:30:02 +0200

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rock-paper-scissors';
+
+  result: String = new String();
+  playerAnswer: String = new String();
+  computerAnswer: Number = new Number();
+
+  getRandomInteger (min: number, max:number){
+    return Math.floor(Math.random() * (max-min + 1)) + min;
+  }
+
+  getResult(n:HTMLInputElement) {
+
+    let comAnswer = this.getRandomInteger(1,3);
+    let answer = n.value;
+
+
+    console.log(comAnswer)
+    console.log(answer)
+
+    if(comAnswer === 1 && answer === "rock" || comAnswer === 2 && answer === "paper" || comAnswer === 3 && answer === "scissors"){
+      this.result = "Drow"
+    } else if (comAnswer === 1 && answer === "paper" || comAnswer === 2 && answer === "rock" ||  comAnswer === 3 && answer === "rock") {
+      this.result = "You Win!"
+    } else if(comAnswer === 1 && answer === "scissors" || comAnswer === 2 && answer === "scissors" ||  comAnswer === 3 && answer === "paper"){
+      this.result = "You Lose!"
+    }
+
+    n.value = ""
+  }
+
+  
+  getResult2(event: Event) {
+    let answer = (event.target as Element).id
+
+    let comAnswer = this.getRandomInteger(1,3);
+
+
+    console.log(answer)
+    console.log(comAnswer)
+
+    if(comAnswer === 1 && answer === "rock" || comAnswer === 2 && answer === "paper" || comAnswer === 3 && answer === "scissors"){
+      this.result = "Drow";
+      this.playerAnswer = answer;
+      this.computerAnswer = comAnswer;
+    } else if (comAnswer === 1 && answer === "paper" || comAnswer === 2 && answer === "rock" ||  comAnswer === 3 && answer === "rock") {
+      this.result = "You Win!"
+      this.playerAnswer = answer;
+      this.computerAnswer = comAnswer;
+    } else if(comAnswer === 1 && answer === "scissors" || comAnswer === 2 && answer === "scissors" ||  comAnswer === 3 && answer === "paper"){
+      this.result = "You Lose!"
+      this.playerAnswer = answer;
+      this.computerAnswer = comAnswer;
+    }
+
+    
+  }
 }
